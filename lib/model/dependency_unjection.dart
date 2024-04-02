85765a23741752031faa3a5ad1eb9603852a10cd
Taha moussa <87451004+moussaart@users.noreply.github.com>
2024-04-02 17:15:05 +0200

    Delete lib directory

--- lib/model/dependency_unjection.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/get_instance.dart';
-import 'package:new_app/Controller/network_controller.dart';
-
-class DependencyInjection {
-  static void init() {
-    Get.put<NetworkController>(NetworkController(), permanent: true);
-  }
-}
